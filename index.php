b781ed40063f50be2b3675c4ce0ed69a4be5eb87
brucknergab <brucknergab2005@gmail.com>
2025-03-18 08:23:36 +0100

    buborékos rendezés kísérlete

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+$tomb=array(11, 5, 4, 8, 7, 9);
+$maxIndex=count($tomb)-1;
+
+//& = referencia
+function csere(&$tomb; $j)
+{
+    $b=$tomb[$j+1];
+    $tomb[$j+1]=$tomb[$j];
+    $tomb[$j]=$b;
+}
+
+for($i=$maxIndex;$i>=1;--)
+{
+    for($j=0; $j<=$i-1;$j++){
+        if($tomb[$j] > $tomb[$j+1]){
+            csere($tomb,$j)
+        }
+    }
+}
+
+print_r($tomb);
